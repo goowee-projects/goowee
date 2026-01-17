025c80a8e4926d9f78e5451d14440044403dddf0
shinjiorisu <shinji.orisu@gmail.com>
2026-01-17 15:14:07 +0100

    Http client package refactoring + server HttpRequest (!!)

--- commons-utils/src/main/groovy/goowee/commons/http/client/HttpResponseType.groovy
+++ commons-utils/src/main/groovy/goowee/commons/http/client/HttpResponseType.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package goowee.commons.http
+package goowee.commons.http.client
 
 /**
  * Defines the type of content returned by an HttpResponse.

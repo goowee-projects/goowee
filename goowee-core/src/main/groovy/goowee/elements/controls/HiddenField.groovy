8627e80327fdbc482f9feb559ad9c67bdfa6b7c4
shinjiorisu <shinji.orisu@gmail.com>
2026-08-05 11:16:47 +0200

    Hidden fields hold IDs from objects with an 'id' field instead of the full JSON structure

--- goowee-core/src/main/groovy/goowee/elements/controls/HiddenField.groovy
+++ goowee-core/src/main/groovy/goowee/elements/controls/HiddenField.groovy
@@ -14,6 +14,7 @@
  */
 package goowee.elements.controls
 
+import goowee.commons.utils.ObjectUtils
 import goowee.elements.core.Control
 import goowee.types.Type
 import goowee.types.Types
@@ -52,6 +53,24 @@ class HiddenField extends Control {
         skipFocus = true
         display = false
         containerSpecs.display = false
+    }
+
+    /**
+     * Sets the selected value(s) for this control.
+     * <ul>
+     *   <li>A single object with an {@code id} property is unwrapped to its ID.</li>
+     *   <li>All other values are passed directly to the superclass.</li>
+     * </ul>
+     *
+     * @param value the value to select; accepts {@code null}, a scalar, or a {@link Collection}
+     */
+    @Override
+    void setValue(Object value) {
+        if (ObjectUtils.hasId(value)) {
+            super.setValue(value['id'])
+        } else {
+            super.setValue(value)
+        }
     }
 
 }

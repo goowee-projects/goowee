1fa3041125f4720daeb36cbe443e940ad89a23e2
shinjiorisu <shinji.orisu@gmail.com>
2026-08-31 12:04:14 +0200

    Darker default highlight color

--- goowee-core/grails-app/services/goowee/tenant/TenantPropertyService.groovy
+++ goowee-core/grails-app/services/goowee/tenant/TenantPropertyService.groovy
@@ -65,7 +65,7 @@ class TenantPropertyService extends PropertyService {
         setString('FRAME_BACKGROUND_COLOR', '#333030', '#333030')
         setString('PRIMARY_TEXT_COLOR', '#ffffff', '#ffffff')
         setString('PRIMARY_BACKGROUND_COLOR', '#255aa8', '#255aa8')
-        setNumber('PRIMARY_BACKGROUND_COLOR_ALPHA', 0.20, 0.20)
+        setNumber('PRIMARY_BACKGROUND_COLOR_ALPHA', 0.30, 0.30)
         setString('SECONDARY_TEXT_COLOR', '#ffffff', '#ffffff')
         setString('SECONDARY_BACKGROUND_COLOR', '#4C4141', '#4C4141')
         setString('REQUIRED_TEXT_COLOR', '#cc0000', '#cc0000')
